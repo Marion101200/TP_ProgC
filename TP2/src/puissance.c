3ca25423982a4404c7e1f389442654231a976e5c
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-12-03 20:34:49 +0100

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;   // Base
+    int b = 3;   // Exposant
+    int resultat = 1;
+
+    // Calcul de a^b
+    for (int i = 0; i < b; i++) {
+        resultat *= a;
+    }
+
+    printf("%d à la puissance %d = %d\n", a, b, resultat);
+
+    return 0;
+}
