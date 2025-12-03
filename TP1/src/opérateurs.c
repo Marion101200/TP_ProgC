ed2009efad34bc24a0bbd8f90e016a7f77d58fb5
Marion101200 <124160461+Marion101200@users.noreply.github.com>
2025-12-03 20:01:18 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    // Opérateurs arithmétiques
+    printf("Addition : a + b = %d\n", a + b);
+    printf("Soustraction : a - b = %d\n", a - b);
+    printf("Multiplication : a * b = %d\n", a * b);
+    printf("Division : a / b = %d\n", a / b);
+    printf("Modulo : a %% b = %d\n\n", a % b);
+
+    // Opérateurs logiques (comparaisons)
+    printf("a == b : %d\n", a == b);
+    printf("a > b : %d\n", a > b);
+
+    return 0;
+}
